184f357d57aab148c4cbd213e7e30e3f9f89814f
jacobazoulay <jacobazoulay@gmail.com>
2023-02-22 17:21:58 -0800

    Added data loader stuff

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,24 @@
 import pandas as pd
 
 
-def parseFile(filepath):
+def parseFile(filepath, transittype="n/a"):
     file1 = open(filepath, 'r')
 
     data = []
     header = ""
     for line in file1:
         if line[:5] == "# Raw":
-            header = line[2:-1].split(",")
+            header = line[2:-1].split(",") + ["TransitType"]
         if line[:3] == "Raw":
-            data.append(line[0:-1].split(","))
+            curRow = line[0:-1].split(",") + [transittype]
+            data.append(curRow)
 
     df = pd.DataFrame(data)
     df.columns = header
 
     return df
 
+
+if __name__ == "__main__":
+    df = parseFile("gnss_log_2023_02_15_16_25_25.txt", "Bike")
+    print(df.head())
