98b78836cbae6ca3a894350e25628505f42b0348
Pol Francesch Huc <pfrances@purdue.edu>
2023-03-07 14:08:56 -0800

    fixed annoying warnings

--- analyze_fix.py
+++ analyze_fix.py
@@ -66,12 +66,15 @@ def average_stop_time(df):
         stop_time = df["UnixTimeMillis"].iloc[b] - df["UnixTimeMillis"].iloc[a]
         stop_times.append(stop_time)
 
-    avg_num_stops = len(stops)/T
-    avg_time_per_stop = np.mean(stop_times)
-    percent_stopped = np.sum(stop_times) / T
-    stop_idxs = stops
+    if len(stop_times) > 0:
+        avg_num_stops = len(stops)/T
+        avg_time_per_stop = np.mean(stop_times)
+        percent_stopped = np.sum(stop_times) / T
+        stop_idxs = stops
 
-    return avg_num_stops, avg_time_per_stop, percent_stopped, stop_idxs
+        return avg_num_stops, avg_time_per_stop, percent_stopped, stop_idxs
+    else:
+        return np.NaN, np.NaN, np.NaN, np.NaN
 
 
 def get_acceleration_features(df):
@@ -168,24 +171,17 @@ def get_features_for_file(df):
 
 def pre_process_files(filePaths, transittype): 
     # Get files
-    # TODO: May want to return this validation set?
-    # Or maybe we call this fcn from somewhere else so we can directly use this validation set?
-    # If we try calling split_data_set again it may not return the same validation set since
-    # it is picked randomly. The randomness is seeded though
-    train, validate = split_data_set(filePaths, validation_percentage=0.1, min_time_before_split=5)
+    train, validate = split_data_set(filePaths, validation_percentage=0.1, min_time_before_split=60)
 
     # Columns: # Stops / s, Avg Stop Duration, Avg Percent Stop Time, Max speed, Avg Speed, Max Accel, Min Accel, Avg Accel
     # Idxs:    0            1                  2                      3          4          5          6          7
-    features = np.zeros((len(train), 8))
-
-    print(transittype + " train: " + str(len(train)) + " and validation: " + str(len(validate)))
-    
+    features = np.zeros((len(train), 8))    
     for idx, Fix_df in enumerate(train):      
         features[idx, :] = get_features_for_file(Fix_df)
 
-    print({"mean": np.nanmean(features, axis=0), "var": np.nanvar(features, axis=0)})
+    print({"transittype": transittype, "# Train sets": len(train), "# Validate sets": len(validate), "mean": np.nanmean(features, axis=0), "var": np.nanvar(features, axis=0)})
 
-    return {"mean": np.nanmean(features, axis=0), "var": np.nanvar(features, axis=0)} 
+    return {"mean": np.nanmean(features, axis=0), "var": np.nanvar(features, axis=0), "validate": validate} 
 
 def crawl():
     #go over each text file in directory
@@ -193,7 +189,7 @@ def crawl():
     bike = pre_process_files(glob.glob(os.path.join(main_dir, "Bike/", "*.csv")), "Bike")    
     car = pre_process_files(glob.glob(os.path.join(main_dir, "Car/", "*.csv")), "Car")
     walk = pre_process_files(glob.glob(os.path.join(main_dir, "Walk/", "*.csv")), "Walk")
-    bus = pre_process_files(glob.glob(os.path.join(main_dir, "Bus/", "*.csv")), "Bus")
+    # bus = pre_process_files(glob.glob(os.path.join(main_dir, "Bus/", "*.csv")), "Bus")
 
 if __name__ == '__main__':
     crawl()
